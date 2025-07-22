22667c55761aca2d29d5570dfeb21b044124be14
Yakub Khan <ysk91949@gmail.com>
2025-07-22 21:06:28 +0530

    Successfully Commited

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,17 @@ dotenv.config({});
 connectToDB();
 
 const app = express();
+const PORT = process.env.BACKEND_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
 
-const PORT = process.env.BACKEND_PORT;
 
 
 app.use("/api/v1/auth", customerRouter);
 app.use("/api/v1/loan", loanRouter);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, '0.0.0.0', ()=>{
     console.log(`Server is running at PORT : ${PORT}`);
 })
